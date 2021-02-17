363dccc425edb72f320fd7c1f0277e7df11401c5
Toporishev Ivan <toporishev.i@medindex.ru>
2021-02-18 00:22:06 +0300

    Добавлены ресурсы кубернетеса

--- FirstService/Program.cs
+++ FirstService/Program.cs
@@ -6,6 +6,6 @@ using Microsoft.AspNetCore.Http;
 WebHost.CreateDefaultBuilder().Configure(app =>
     {
         app.UseRouting();
-        app.UseEndpoints(e => { e.MapGet("/health/", c => c.Response.WriteAsync("{'status': 'OK'}")); });
+        app.UseEndpoints(e => { e.MapGet("/health", c => c.Response.WriteAsync("{'status': 'OK'}")); });
     })
     .Build().Run();
